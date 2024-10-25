4291bb785f0481d6e5bc2b9ea5deafed340cc49d
pra-navv <pranavwagholikar09@gmail.com>
2024-10-25 16:09:14 +0530

    Storing Data In Database

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
-
+from .models import Form
+from django.contrib import messages
 
 def index(request):
     if request.method == "POST":
@@ -11,4 +12,8 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-    return render(request,"index.html")
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                               email=email, date=date, occupation=occupation)
+            messages.success(request, "Form submitted successfully!")
+    return render(request, "index.html")
