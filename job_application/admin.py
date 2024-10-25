e84e959bd6f1509042420ee0d564ca93827f719c
pra-navv <pranavwagholikar09@gmail.com>
2024-10-25 17:45:21 +0530

    admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email",
+                    "occupation", "date")
+    search_fields = ("first_name", "last_name", "email",
+                    "occupation", "date")
+    list_filter = ("occupation", "date")
+    ordering = ("first_name",)
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
 
